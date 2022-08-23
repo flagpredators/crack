f1cdb83639db9c21d1e2497f7a1648f8dfc2a236
SynAcktraa <harshmetaexploit90@gmail.com>
2022-08-23 23:28:59 +0530

    chore: added beaufort algorithm

--- beaufort.py
+++ beaufort.py
@@ -1,10 +1,9 @@
-
 import sys
 
 LET = 26
 UTL = 32
 
-class Vignere():
+class Beaufort():
 
     def __init__(self, cred: str, key: str) -> None:
         self.cred = cred
@@ -37,8 +36,8 @@ class Vignere():
                 j += 1
             
         return gen_key;
-    
-    def encrypt(self, genkey: str) -> str:
+
+    def eval(self, genkey: str) -> str:
 
         ciphertext = str()
         for idx, char in enumerate(self.cred):
@@ -46,31 +45,14 @@ class Vignere():
             cache = ord(char.upper())
             if cache >=65 and cache <=90:
                 flag = UTL if char.islower() else 0;
-                buffer = cache - (65 - ord(list(genkey)[idx].upper())) + flag;
-                buffer -= (LET if buffer > 122 else 0)
-                ciphertext += chr(buffer)
-            else:
-                ciphertext += char
-        
-        return ciphertext
-
-    def decrypt(self, genkey: str) -> str:
-
-        ciphertext = str()
-        for idx, char in enumerate(self.cred):
-
-            cache = ord(char.upper())
-            if cache >=65 and cache <=90:
-                flag = UTL if char.islower() else 0;
-                buffer = cache - (ord(list(genkey)[idx].upper()) - 65) ;
-                buffer += (LET if buffer < 65 else 0)
+                buffer = ord(list(genkey)[idx].upper()) - cache
+                buffer += (LET if buffer < 0 else 0) + 65
                 ciphertext += chr(buffer+flag)
             else:
                 ciphertext += char
         
         return ciphertext
 
-# instance = Vignere(sys.argv[1], sys.argv[2])
-# key = instance.keygen()
-# print(key)
-# print(instance.decrypt(key))
+instance = Beaufort(sys.argv[1], sys.argv[2])
+key = instance.keygen()
+print(instance.eval(key))
